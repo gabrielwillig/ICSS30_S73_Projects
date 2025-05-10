c31b3515999f3f5858be93349813bdd53fa5b379
luisteod <luislopesteo@hotmail.com>
2025-05-10 20:56:49 -0300

    script de inicializaoção

--- 3/p2p/run.sh
+++ 3/p2p/run.sh
@@ -1,6 +1,7 @@
 cd src
-# python -m peer peer-1 &
-# python -m peer peer-2 &
-# python -m peer peer-3 &
-# python -m peer peer-4 & 
+pyro5-ns &
+python -m peer peer-1 &
+python -m peer peer-2 &
+python -m peer peer-3 &
+python -m peer peer-4 & 
 python -m peer peer-5 
